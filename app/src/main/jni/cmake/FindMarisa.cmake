c3fe75b8ef3c105701d1f9348bdcacfd1a7ca86a
kingzcheung <kingzcheung@gmail.com>
2026-03-29 20:03:48 +0800

    feat: add cmake config files for CI build

---
+++ app/src/main/jni/cmake/FindMarisa.cmake
@@ -0,0 +1,5 @@
+# FindMarisa.cmake - Custom finder for marisa-trie built via add_subdirectory
+
+set(Marisa_FOUND TRUE)
+set(Marisa_INCLUDE_PATH "${CMAKE_SOURCE_DIR}/librime/deps/marisa-trie/include")
+set(Marisa_LIBRARY marisa)
---
+++ app/src/main/jni/cmake/FindOpencc.cmake
@@ -0,0 +1,5 @@
+# FindOpencc.cmake - Custom finder for OpenCC built via add_subdirectory
+
+set(Opencc_FOUND TRUE)
+set(Opencc_INCLUDE_PATH "${CMAKE_SOURCE_DIR}/librime/deps/opencc/src")
+set(Opencc_LIBRARY opencc)
